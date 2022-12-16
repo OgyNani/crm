5c2488672e12177cd50d263eb35393beb6bf5478
ogynani <ogynani@play.me>
2022-12-16 20:35:04 +0200

    logout

--- src/Controller/Login.php
+++ src/Controller/Login.php
@@ -23,4 +23,11 @@ class Login extends AbstractController
             'error'        => $error,
         ]);
     }
+
+    #[Route('/logout', name: 'logout', methods: ['GET'])]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
